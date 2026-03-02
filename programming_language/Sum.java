095603c37d7663ab5f8b5730f9923618ea4429f4
vishal <ashm7112006.gmail.com>
2026-03-02 09:16:57 +0530

    Contents are inserted

---
+++ programming_language/Sum.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+class P1
+{
+   public static void main (String args[])
+   {
+     int a;
+     int b;
+     Scanner S=new Scanner(system.in);
+     System.out.println("Enter value for a:")
+     System.out.println("Enter value for b:")
+     int Sum=a+b;
+     System.out.println("Sum="+Sum);
+   }
+}
